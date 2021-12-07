c14bd7e0abf081d87e2969fc673a82c80e49c00e
ECR_EXCALIBUR_G770 <ECR_EXCALIBUR_G770@DESKTOP-TUMHS1A>
2021-12-07 17:11:22 +0300

    Add project files.

---
+++ MCD_GenericOncesiIslemler/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace MCD_GenericOncesiIslemler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ArrayList liste = new ArrayList();
+            //liste.Add();
+
+            SanalDatabase sdatabase = new SanalDatabase();
+            sdatabase.yeniKayit(1);
+            sdatabase.yeniKayit(2);
+        }
+    }
+}
---
+++ MCD_GenericOncesiIslemler/SanalDatabase.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace MCD_GenericOncesiIslemler
+{
+    public class SanalDatabase
+    {
+        private ArrayList Listem;
+
+        public SanalDatabase()
+        {
+            Listem = new ArrayList();
+        }
+
+        public void yeniKayit(int data)
+        {
+            Listem.Add(data);
+        }
+    }
+}
